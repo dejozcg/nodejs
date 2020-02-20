07a8c045ee62e42a7693c952b9dac64bd890cd1d
dejozcg <dejozcg@yahoo.com>
2020-02-20 13:51:25 +0100

    a

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
+import { json } from "body-parser";
+import { stat } from "fs";
+
 /*
  * Frontend Logic for application
  *
@@ -26,63 +29,57 @@ app.client.request = function(headers,path,method,queryStringObject,payloads,cal
     callback = typeof(callback) == 'function' ? callback : false;
 
     // For each query string parameter sent, add it to the path
-    
-};
+    var requestUrl= path + '?';
+    var counter = 0;
 
-app.client.request = function(headers,path,method,queryStringObject,payload,callback){
-  // For each query string parameter sent, add it to the path
-  var requestUrl = path+'?';
-  var counter = 0;
-  for(var queryKey in queryStringObject){
-     if(queryStringObject.hasOwnProperty(queryKey)){
-       counter++;
-       // If at least one query string parameter has already been added, preprend new ones with an ampersand
-       if(counter > 1){
-         requestUrl+='&';
-       }
-       // Add the key and value
-       requestUrl+=queryKey+'='+queryStringObject[queryKey];
-     }
-  }
-
-  // Form the http request as a JSON type
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, requestUrl, true);
-  xhr.setRequestHeader("Content-type", "application/json");
-
-  // For each header sent, add it to the request
-  for(var headerKey in headers){
-     if(headers.hasOwnProperty(headerKey)){
-       xhr.setRequestHeader(headerKey, headers[headerKey]);
-     }
-  }
-
-  // If there is a current session token set, add that as a header
-  if(app.config.sessionToken){
-    xhr.setRequestHeader("token", app.config.sessionToken.id);
-  }
-
-  // When the request comes back, handle the response
-  xhr.onreadystatechange = function() {
-      if(xhr.readyState == XMLHttpRequest.DONE) {
-        var statusCode = xhr.status;
-        var responseReturned = xhr.responseText;
-
-        // Callback if requested
-        if(callback){
-          try{
-            var parsedResponse = JSON.parse(responseReturned);
-            callback(statusCode,parsedResponse);
-          } catch(e){
-            callback(statusCode,false);
-          }
-
+    for(var queryKey in queryStringObject){
+        if(queryStringObject.hasOwnProperty(queryKey)){
+            counter++;
+            // If at least one query string parameter has already been added, preprend new ones with an ampersand
+            if(counter > 1){
+                requestUrl+='&';
+            }
+            // Add the key and value
+            requestUrl+=queryKey+'='+queryStringObject[queryKey];
         }
-      }
-  }
+    }
 
-  // Send the payload as JSON
-  var payloadString = JSON.stringify(payload);
-  xhr.send(payloadString);
+    // Form the http request as a JSON type
+    var xhr = new XMLHttpRequest();
+    xhr.open(method,requestUrl,true);
+    xhr.setRequestHeader('Content-type', 'application/json');
 
+    // For each header sent, add it to the request
+    for(var headerKey in headers){
+        if(headers.hasOwnProperty(headerKey)){
+            xhr.setRequestHeader(headerKey, headers[headerKey]);
+        }
+    }
+
+    // If there is a current session token set, add that as a header
+    if(app.config.sessionToken){
+        xhr.setRequestHeader('token', app.config.sessionToken.id);
+    }
+
+    // When the request comes back, handle the response
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            var statusCode = xhr.status;
+            var responseReturned = xhr.responseText;
+
+            // Callback if requested
+            if(callback){
+                try{
+                    var parsedResponse = JSON.parse(responseReturned);
+                    callback(statusCode,parsedResponse);
+                }catch(e){
+                    callback(statusCode,false);
+                }
+            }
+        }
+    };
+
+    // Send the payload as JSON
+    var payloadString = JSON.stringify(payloads);
+    xhr.send(payloadString);
 };
